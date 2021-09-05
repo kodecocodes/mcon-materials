4a95e6ec5b222d5e27a28815221cd1f033655abb
Richard Turton <jrturton@gmail.com>
2021-09-05 14:30:51 +0100

    Challenge project after TE

--- 04-async-streams/projects/challenge/BlabberTests/Utility/TestURLProtocol.swift
+++ 04-async-streams/projects/challenge/BlabberTests/Utility/TestURLProtocol.swift
@@ -37,26 +37,23 @@ class TestURLProtocol: URLProtocol {
   override class func canInit(with request: URLRequest) -> Bool {
     return true
   }
-  
+
   override class func canonicalRequest(for request: URLRequest) -> URLRequest {
     return request
   }
-  
+
   /// Store the URL request and send success response back to the client.
   override func startLoading() {
     guard let client = client,
-          let url = request.url else { fatalError() }
-    
-    let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+      let url = request.url,
+      let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+      else { fatalError("Client or URL missing") }
+
     client.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
     client.urlProtocol(self, didLoad: Data())
     client.urlProtocolDidFinishLoading(self)
-    
-    
   }
-  
+
   override func stopLoading() {
-    
   }
-  
 }

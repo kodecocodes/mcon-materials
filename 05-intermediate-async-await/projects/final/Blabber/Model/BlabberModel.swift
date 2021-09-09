b0ee1599ae5c2bec585cfb970ff0c6e4303d2dbf
Richard Turton <jrturton@gmail.com>
2021-09-09 23:37:10 +0100

    TE complete up to challenge

--- 05-intermediate-async-await/projects/final/Blabber/Model/BlabberModel.swift
+++ 05-intermediate-async-await/projects/final/Blabber/Model/BlabberModel.swift
@@ -39,31 +39,27 @@ import UIKit
 class BlabberModel: ObservableObject {
   var username = ""
   var urlSession = URLSession.shared
-  
+
   init() {
-    
   }
-  
+
   /// Current live updates
-  @Published var messages = [Message]()
-  
+    @Published var messages: [Message] = []
+
   /// Shares the current user's address in chat.
   func shareLocation() async throws {
     let manager = CLLocationManager()
     manager.requestWhenInUseAuthorization()
-    guard [CLAuthorizationStatus.authorizedAlways, .authorizedWhenInUse].contains(manager.authorizationStatus) else {
-      throw "The app isn't authorized to use location data"
-    }
-    
-    var delegate: ChatLocationDelegate? = nil
-    let location: CLLocation = try await withCheckedThrowingContinuation { continuation in
+
+    var delegate: ChatLocationDelegate?
+    let location: CLLocation = try await
+    withCheckedThrowingContinuation { continuation in
       delegate = ChatLocationDelegate(continuation: continuation)
       manager.delegate = delegate
-      manager.startUpdatingLocation()
     }
     print(location.description)
-    
-    let address: String = try await withCheckedThrowingContinuation({ continuation in
+    let address: String = try await
+    withCheckedThrowingContinuation { continuation in
       AddressEncoder.addressFor(location: location) { address, error in
         switch (address, error) {
         case (nil, .some(let error)):
@@ -72,75 +68,57 @@ class BlabberModel: ObservableObject {
           continuation.resume(returning: address)
         case (nil, nil):
           continuation.resume(throwing: "Address encoding failed")
-        case (.some(let address), .some(let error)):
+        case let (.some(address), .some(error)):
           continuation.resume(returning: address)
           print(error)
         }
       }
-    })
-    
+    }
     try await say("📍 \(address)")
   }
-  
+
   /// Does a countdown and sends the message.
   func countdown(to message: String) async throws {
     guard !message.isEmpty else { return }
-    
-    //    let counter = AsyncStream(String.self) { continuation in
-    //      var count = 3
-    //      Timer.scheduledTimer(withTimeInterval: 1.0,
-    //        repeats: true) { timer in
-    //        guard count > 0 else {
-    //          timer.invalidate()
-    //          continuation.yield(with: .success("🎉 " + message))
-    //          return
-    //        }
-    //
-    //        continuation.yield("\(count) ...")
-    //        count -= 1
-    //      }
-    //    }
-    
-    var count = 3
+    var countdown = 3
     let counter = AsyncStream<String> {
       do {
         try await Task.sleep(nanoseconds: 1_000_000_000)
       } catch {
         return nil
       }
-      
-      defer { count -= 1 }
-      
-      switch count {
-      case 3, 2, 1 : return "\(count)..."
+
+      defer { countdown -= 1 }
+
+      switch countdown {
+      case (1...): return "\(countdown)..."
       case 0: return "🎉 " + message
       default: return nil
       }
     }
-    
-    //    for await countdownMessage in counter {
-    //      try await say(countdownMessage)
-    //    }
+
     try await counter.forEach { [weak self] in
       try await self?.say($0)
     }
   }
-  
+
   /// Start live chat updates
   @MainActor
   func chat() async throws {
-    guard let query = username.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+    guard
+      let query = username.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+      let url = URL(string: "http://localhost:8080/chat/room?\(query)")
+      else {
       throw "Invalid username"
     }
-    let url = URL(string: "http://localhost:8080/chat/room?\(query)")!
-    
+
     let (stream, response) = try await liveURLSession.bytes(from: url, delegate: nil)
     guard (response as? HTTPURLResponse)?.statusCode == 200 else {
       throw "The server responded with an error."
     }
-    
+
     print("Start live updates")
-    
+
     try await withTaskCancellationHandler {
       print("End live updates")
       messages = []
@@ -148,21 +126,22 @@ class BlabberModel: ObservableObject {
       try await readMessages(stream: stream)
     }
   }
-  
+
   /// Reads the server chat stream and updates the data model.
   @MainActor
   private func readMessages(stream: URLSession.AsyncBytes) async throws {
     var iterator = stream.lines.makeAsyncIterator()
-    
+
     guard let first = try await iterator.next() else {
       throw "No response from server"
     }
-    
-    guard let data = first.data(using: .utf8),
-          let status = try? JSONDecoder().decode(ServerStatus.self, from: data) else {
-            throw "Invalid response from server"
-          }
-    
+    guard
+      let data = first.data(using: .utf8),
+      let status = try? JSONDecoder().decode(ServerStatus.self, from: data)
+    else {
+      throw "Invalid response from server"
+    }
+
     messages.append(
       Message(
         id: UUID(),
@@ -171,55 +150,58 @@ class BlabberModel: ObservableObject {
         date: Date()
       )
     )
-    
+
     let notifications = Task {
       await observeAppStatus()
     }
+
     defer {
       notifications.cancel()
     }
-    
+
     for try await line in stream.lines {
       if let data = line.data(using: .utf8),
-         let update = try? JSONDecoder()
-          .decode(Message.self, from: data) {
-        
+        let update = try? JSONDecoder().decode(Message.self, from: data) {
         messages.append(update)
       }
     }
   }
-  
-  func observeAppStatus() async {
-    Task {
-      for await _ in await NotificationCenter.default.notifications(for: UIApplication.willResignActiveNotification) {
-        try? await say("\(username) went away", isSystemMessage: true)
-      }
-    }
-    
-    Task {
-      for await _ in await NotificationCenter.default.notifications(for: UIApplication.didBecomeActiveNotification) {
-        try? await say("\(username) came back", isSystemMessage: true)
-      }
-    }
-  }
-  
+
   /// Sends the user's message to the chat server
   func say(_ text: String, isSystemMessage: Bool = false) async throws {
-    guard !text.isEmpty else { return }
-    let url = URL(string: "http://localhost:8080/chat/say")!
-    
+    guard
+      !text.isEmpty,
+      let url = URL(string: "http://localhost:8080/chat/say")
+    else { return }
+
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
     request.httpBody = try JSONEncoder().encode(
       Message(id: UUID(), user: isSystemMessage ? nil : username, message: text, date: Date())
     )
-    
+
     let (_, response) = try await urlSession.data(for: request, delegate: nil)
     guard (response as? HTTPURLResponse)?.statusCode == 200 else {
       throw "The server responded with an error."
     }
   }
-  
+
+  func observeAppStatus() async {
+    Task {
+      for await _ in await NotificationCenter.default
+        .notifications(for: UIApplication.willResignActiveNotification) {
+        try? await say("\(username) went away", isSystemMessage: true)
+      }
+    }
+
+    Task {
+      for await _ in await NotificationCenter.default
+        .notifications(for: UIApplication.didBecomeActiveNotification) {
+        try? await say("\(username) came back", isSystemMessage: true)
+      }
+    }
+  }
+
   /// A URL session that goes on indefinitely, receiving live updates.
   private var liveURLSession: URLSession = {
     var configuration = URLSessionConfiguration.default
@@ -229,9 +211,7 @@ class BlabberModel: ObservableObject {
 }
 
 extension AsyncSequence {
-  func forEach(_
-               block: (Element) async throws -> Void) async throws {
-    
+  func forEach(_ block: (Element) async throws -> Void) async throws {
     for try await element in self {
       try await block(element)
     }
--- 05-intermediate-async-await/projects/final/Blabber/Utility/AddressEncoder.swift
+++ 05-intermediate-async-await/projects/final/Blabber/Utility/AddressEncoder.swift
@@ -35,21 +35,21 @@ import Contacts
 
 /// A type that converts a location into a human readable address.
 enum AddressEncoder {
-  
   /// Converts the given location into the nearest address, calls `completion` when finished.
   ///
   /// - Note: This method is "simulating" an old-style callback API that the reader can wrap
   ///   as an async code while working through the book.
   static func addressFor(location: CLLocation, completion: @escaping (String?, Error?) -> Void) {
     let geocoder = CLGeocoder()
-    
+
     Task {
       do {
-        guard let placemark = try await geocoder.reverseGeocodeLocation(location).first,
-              let address = placemark.postalAddress else {
-                completion(nil, "No addresses found")
-                return
-              }
+        guard
+          let placemark = try await geocoder.reverseGeocodeLocation(location).first,
+          let address = placemark.postalAddress else {
+            completion(nil, "No addresses found")
+            return
+          }
         completion(CNPostalAddressFormatter.string(from: address, style: .mailingAddress), nil)
       } catch {
         completion(nil, error)

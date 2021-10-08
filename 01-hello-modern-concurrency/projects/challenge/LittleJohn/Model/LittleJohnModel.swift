323fb406c4cac0a0c7cf1bedcb96fdac95b5d72f
Shai Mishali <freak4pc@gmail.com>
2021-10-09 00:00:15 +0300

    Ch1 Challenge

--- 01-hello-modern-concurrency/projects/challenge/LittleJohn/Model/LittleJohnModel.swift
+++ 01-hello-modern-concurrency/projects/challenge/LittleJohn/Model/LittleJohnModel.swift
@@ -37,17 +37,6 @@ extension String: Error { }
 
 /// The app model that communicates with the server.
 class LittleJohnModel: ObservableObject {
-  func availableSymbols() async throws -> [String] {
-    guard let url = URL(string: "http://localhost:8080/littlejohn/symbols") else {
-      throw "The URL could not be created."
-    }
-
-    let (data, response) = try await URLSession.shared.data(from: url, delegate: nil)
-    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-      throw "The server responded with an error."
-    }
-    return try JSONDecoder().decode([String].self, from: data)
-  }
   /// Current live updates.
   @Published private(set) var tickerSymbols: [Stock] = []
 
@@ -57,24 +46,44 @@ class LittleJohnModel: ObservableObject {
     guard let url = URL(string: "http://localhost:8080/littlejohn/ticker?\(selectedSymbols.joined(separator: ","))") else {
       throw "The URL could not be created."
     }
-    let (stream, response) = try await liveURLSession.bytes(from: url, delegate: nil)
+
+    let (stream, response) = try await liveURLSession.bytes(from: url)
+
     guard (response as? HTTPURLResponse)?.statusCode == 200 else {
       throw "The server responded with an error."
     }
+
     for try await line in stream.lines {
-      if let data = line.data(using: .utf8),
-        let update = try? JSONDecoder().decode([Stock].self, from: data) {
-        await MainActor.run {
-          tickerSymbols = update.sorted { $0.name < $1.name }
-          print("Updated: \(Date())")
-        }
+      let sortedSymbols = try JSONDecoder()
+        .decode([Stock].self, from: Data(line.utf8))
+        .sorted(by: { $0.name < $1.name })
+
+      await MainActor.run {
+        tickerSymbols = sortedSymbols
+        print("Updated: \(Date())")
       }
     }
+
     // Challenge code.
     await MainActor.run {
       tickerSymbols = []
     }
   }
+
+  func availableSymbols() async throws -> [String] {
+    guard let url = URL(string: "http://localhost:8080/littlejohn/symbols") else {
+      throw "The URL could not be created."
+    }
+
+    let (data, response) = try await URLSession.shared.data(from: url)
+
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw "The server responded with an error."
+    }
+
+    return try JSONDecoder().decode([String].self, from: data)
+  }
+
   /// A URL session that lets requests run indefinitely so we can receive live updates from server.
   private lazy var liveURLSession: URLSession = {
     var configuration = URLSessionConfiguration.default

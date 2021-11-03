547f0324a030abdddf5fda38ac71dbd10dd392d6
Marin Todorov <marin@underplot.com>
2021-11-03 10:05:37 +0100

    tabs -> spaces

--- 00-book-server/Package.swift
+++ 00-book-server/Package.swift
@@ -2,24 +2,24 @@
 import PackageDescription
 
 let package = Package(
-	name: "BookServer",
-	platforms: [
-		.macOS(.v10_15)
-	],
-	dependencies: [
-		// 💧 A server-side Swift web framework.
-		.package(url: "https://github.com/vapor/vapor.git", .exact("4.49.0")),
-	],
-	targets: [
-		.target(
-			name: "App",
-			dependencies: [
-				.product(name: "Vapor", package: "vapor")
-			],
-			swiftSettings: [
-				.unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-			]
-		),
-		.executableTarget(name: "Run", dependencies: [.target(name: "App")])
-	]
+  name: "BookServer",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  dependencies: [
+    // 💧 A server-side Swift web framework.
+    .package(url: "https://github.com/vapor/vapor.git", .exact("4.49.0")),
+  ],
+  targets: [
+    .target(
+      name: "App",
+      dependencies: [
+        .product(name: "Vapor", package: "vapor")
+      ],
+      swiftSettings: [
+        .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+      ]
+    ),
+    .executableTarget(name: "Run", dependencies: [.target(name: "App")])
+  ]
 )

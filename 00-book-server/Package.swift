261aee111736b5c7713cb6adbd5afde2b5fceab1
Nikolay Dimitrov <tehnsd@gmail.com>
2024-01-26 21:38:45 +0200

    Raised Vapor package version number

--- 00-book-server/Package.swift
+++ 00-book-server/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
   ],
   dependencies: [
     // 💧 A server-side Swift web framework.
-    .package(url: "https://github.com/vapor/vapor.git", .exact("4.66.1")),
+    .package(url: "https://github.com/vapor/vapor.git", .exact("4.92.1")),
   ],
   targets: [
     .target(
